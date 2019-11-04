4e90dce8509eaee223a5776b0ca14e576706c09f
Charles Godwin <20710431+CharlesGodwin@users.noreply.github.com>
2019-11-04 15:31:48 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../magnum'))
-import magnum
+
 
 # -- Project information -----------------------------------------------------
 
@@ -30,7 +30,6 @@ release = '1.0.a2'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -53,6 +52,8 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+master_doc = 'index'
 
 
 # -- Extension configuration -------------------------------------------------
