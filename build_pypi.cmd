af01e02a81206581f486a72b99dc0117ca902eac
Charles Godwin <charles@godwin.ca>
2020-06-08 13:52:01 -0400

    fixed build help text

--- build_pypi.cmd
+++ build_pypi.cmd
@@ -5,3 +5,4 @@ python setup.py sdist bdist_wheel
 python -m twine upload -u CharlesGodwin dist/*
 @echo Use this to install
 @echo sudo pip3 install --upgrade pymagnum
+@echo sudo pips install --upgrade  pymagnum==VERSION
