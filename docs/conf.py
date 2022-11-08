7ab46705ebe3d167e574a27fe4b712d82a124254
Charles Godwin <charles@godwin.ca>
2022-11-08 11:21:28 -0500

    Cleaned up documentation (#46)

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = 'Charles Godwin'
 
 # The full version, including alpha/beta/rc tags
 release = '2.0'
-version = '2.0'
+version = '2.0.2'
 
 # -- General configuration ---------------------------------------------------
 
